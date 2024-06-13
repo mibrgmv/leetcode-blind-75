24d3531d522828ea165f84bd6dba9620133380ef
mibrgmv <mibrgmv@gmail.com>
2024-06-13 18:22:46 +0300

    progress check – 0020--valid-parentheses

---
+++ src/valid_parentheses__0020/Solution.java
@@ -0,0 +1,20 @@
+package valid_parentheses__0020;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (!stack.empty() &&
+                    ((c == ')' && stack.peek() == '(') || (c == ']' && stack.peek() == '[') || (c == '}' && stack.peek() == '{'))) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
