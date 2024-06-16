4f7f96caad0a6a5da8a009b432abfb220cebe3ef
mibrgmv <113512428+mibrgmv@users.noreply.github.com>
2024-06-16 03:03:24 +0300

    Update Solution.java

--- src/three_sum__0015/Solution.java
+++ src/three_sum__0015/Solution.java
@@ -34,8 +34,4 @@ class Solution {
 
         return result;
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Solution().threeSum(new int[] {-1,0,1,2,-1,-4}));
-    }
 }
