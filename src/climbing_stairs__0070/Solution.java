1ff0a74d6a04887c5428c5a9731b3f64736076e0
mibrgmv <mibrgmv@gmail.com>
2024-06-16 03:47:12 +0300

    climbing-stairs--0070

---
+++ src/climbing_stairs__0070/Solution.java
@@ -0,0 +1,14 @@
+package climbing_stairs__0070;
+
+import java.util.Arrays;
+
+class Solution {
+    int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 0, 2, 1);
+        for (int i = 2; i < n + 1; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
